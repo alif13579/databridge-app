269f68cc13a51461a6f2b9e7e66fe8b54266490e
Alif Mia <alif@cloudx.dev>
2026-07-27 00:18:32 +0000

    chore: bump version to 172 / 5.22.19

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.cloudx.databridge"
         minSdk = 23
         targetSdk = 34
-        versionCode = 171
-        versionName = "5.22.18"
+        versionCode = 172
+        versionName = "5.22.19"
         multiDexEnabled = true                                         // ✅ Large app support
         vectorDrawables.useSupportLibrary = true       // ✅ Vector drawable on API 21+
     }
